2bcae3d8b24435326514f1594690aa6bbd6a481c
Rajesh Panchal <rajeshp22@gmail.com>
2019-10-09 15:33:03 +0530

    Testimonial text added

--- testimonials.php
+++ testimonials.php
@@ -1,6 +1,6 @@
 <section class="hero">
     <div class="hero-body" style="padding-bottom:7px !important">
-        <div class="container" >
+        <div class="container">
             <div class="columns">
                 <div class="column is-8">
                     <h1 class="title">
@@ -8,35 +8,35 @@
                     </h1>
                     <!--Testimonial Text Starts-->
                     <article class="media">
-                      <figure class="media-left">
-                        <p class="image is-64x64">
-                          <img class="is-rounded" src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/NafSadh_Profile.jpg/768px-NafSadh_Profile.jpg">
-                        </p>
-                      </figure>
-                      <div class="media-content">
-                        <div class="content">
-                          <p style="font-size:11pt">
-                            <strong>John Smith</strong>
-                            <br>
-                            <span class="is-dark">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
-                            </span>
-                          </p>
+                        <figure class="media-left">
+                            <p class="image is-64x64">
+                                <img class="is-rounded" src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a1/NafSadh_Profile.jpg/768px-NafSadh_Profile.jpg">
+                            </p>
+                        </figure>
+                        <div class="media-content">
+                            <div class="content">
+                                <p style="font-size:11pt">
+                                    <strong>Lawrence Smith</strong>
+                                    <br>
+                                    <span class="is-dark">
+                                        I have been using number of job opportunity portals mainly freelancers kind of jobs but most of them are paid or there will be somewhere a hidden charges will be applicable when it comes to in cash the opportunity. But with the WeherSert, I am totally amazed to know that it is absolutely free. They even don't restrict you while upload your profile in details. I am highly impress!
+                                    </span>
+                                </p>
+                            </div>
+                            <nav class="level is-mobile">
+                                <div class="level-left">
+                                    <a class="level-item">
+                                        <span class="icon is-small"><i class="fas fa-reply"></i></span>
+                                    </a>
+                                    <a class="level-item">
+                                        <span class="icon is-small"><i class="fas fa-retweet"></i></span>
+                                    </a>
+                                    <a class="level-item">
+                                        <span class="icon is-small"><i class="fas fa-heart"></i></span>
+                                    </a>
+                                </div>
+                            </nav>
                         </div>
-                        <nav class="level is-mobile">
-                          <div class="level-left">
-                            <a class="level-item">
-                              <span class="icon is-small"><i class="fas fa-reply"></i></span>
-                            </a>
-                            <a class="level-item">
-                              <span class="icon is-small"><i class="fas fa-retweet"></i></span>
-                            </a>
-                            <a class="level-item">
-                              <span class="icon is-small"><i class="fas fa-heart"></i></span>
-                            </a>
-                          </div>
-                        </nav>
-                      </div>
                     </article>
                     <!--Testimonial Text Ends-->
                 </div>
