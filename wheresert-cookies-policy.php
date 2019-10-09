5371a705308c1b0db42595465956b18cdb3144a2
wndeepak <deepaknishad31@gmail.com>
2019-10-09 13:09:40 +0530

    cookies and copyright added

--- wheresert-cookies-policy.php
+++ wheresert-cookies-policy.php
@@ -22,11 +22,78 @@
     <section class="section greenSection" style="margin-top:50px">
         <div class="container">
             <h2 class="subtitle">
-                WhereSert Cookies Policy
+                Cookies Policy
             </h2>
         </div>
     </section>
 
+    <section class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column">
+
+                    <p><b>WhereSert</b> ("us", "we", or "our") uses cookies on the https://wheresert.com/
+                        website and the wheresert mobile application (the "Service"). By using the
+                        Service, you consent to the use of cookies.</p>
+                    <br>
+
+                    <p>Our Cookies Policy explains what cookies are, how we use cookies, how third-
+                        parties we may partner with may use cookies on the Service, your choices
+                        regarding cookies and further information about cookies.</p>
+                    <br>
+
+                    <p><b>What are cookies</b><br> Cookies are small pieces of text sent by your web browser by a website you
+                        visit. A cookie file is stored in your web browser and allows the Service or a
+                        third-party to recognize you and make your next visit easier and the Service
+                        more useful to you.</p>
+                    <br>
+                    <p>Cookies can be "persistent" or "session" cookies. Persistent cookies remain on
+                        your personal computer or mobile device when you go offline, while session
+                        cookies are deleted as soon as you close your web browser.</p>
+
+                    <br>
+
+
+
+                    <p><b>How wheresert uses cookies</b></p>
+                    <p>When you use and access the Service, we may place a number of cookies files in
+                        your web browser.</p>
+                    <br>
+
+                    We use cookies for the following purposes:
+                    <br>
+
+                    * To enable certain functions of the Service
+                    <br>
+
+                    <b>1.</b>We use both session and persistent cookies on the Service and we use
+                    different types of cookies to run the Service:
+                    <br>
+
+                    <b>2.</b>Essential cookies. We may use essential cookies to authenticate users and
+                    prevent fraudulent use of user accounts.
+                    <br>
+                    <br>
+
+                    <b>What are your choices regarding cookies</b>
+                    <br>
+
+                    If you'd like to delete cookies or instruct your web browser to delete or
+                    refuse cookies, please visit the help pages of your web browser. As an
+                    European citizen, under GDPR, you have certain individual rights. You can
+                    learn more about these rights in the [GDPR
+                    Guide]
+                    <br>
+                    Please note, however, that if you delete cookies or refuse to accept them, you
+                    might not be able to use all of the features we offer, you may not be able to
+                    store your preferences, and some of our pages might not display properly.
+                    <br>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
     <!--Footer Starts-->
     <?php include_once('footer.php'); ?>
     <!--Footer Ends-->
--- wheresert-copyrights-notifications.php
+++ wheresert-copyrights-notifications.php
@@ -22,8 +22,40 @@
     <section class="section greenSection" style="margin-top:50px">
         <div class="container">
             <h2 class="subtitle">
-                WhereSert Copy Notifications
+                Copyrights Notifications
             </h2>
+        </div>
+    </section>
+
+    <section class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column">
+                    Copyright © WhereSert 2019 All Rights Reserved
+                    <br>
+                    <br>
+                    <p class="content has-text-justified">
+                        <p class="title is-4">Notice of Copyright</p>
+                        1. All files and information contained in this Website or Blog are copyright by WhereSert, and may not be duplicated, copied, modified or adapted, in any way without our written permission.
+                        <br>
+                        <br>
+                        2. Our Website or Blog may contain our service marks or trademarks as well as those of our affiliates or other companies, in the form of words, graphics, and logos.
+                        <br>
+                        <br>
+                        3. Your use of our Website, Blog or Services does not constitute any right or license for you to use our service marks or trademarks, without the prior written permission of WhereSert.
+                        <br>
+                        <br>
+                        4. The copying, redistribution, use or publication by you of any such Content, is strictly prohibited.
+                        <br>
+                        <br>
+                        5. Your use of our Website and Services does not grant you any ownership rights to our Content.
+                    </p>
+                    <br>
+                    <br>
+
+                    Copyright © WhereSert. All Rights Reserved. www.wheresert.com
+                </div>
+            </div>
         </div>
     </section>
 
