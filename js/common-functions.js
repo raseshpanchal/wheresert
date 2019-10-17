2569c2f4ce1d8b8bcc9db902c18d218e8878c3a6
Rajesh Panchal <rajeshp22@gmail.com>
2019-10-17 13:28:35 +0530

    Additional function for image upload

--- js/common-functions.js
+++ js/common-functions.js
@@ -1,27 +1,3 @@
-//PDF Uploading Function
-function profilePDF() {
-    // function from the jquery form plugin
-    $('#myFormPdf').ajaxForm({
-        beforeSend: function () {
-            $('#fileStatus').html('Please wait...');
-        },
-        uploadProgress: function (event, position, total, percentComplete) {
-            $('#fileStatus').html('File transfer is going on. Please wait...');
-        },
-        success: function () {
-            $('#fileStatus').html('File uploaded successfully!');
-        },
-        complete: function (response) {
-            if (response.responseText == '0') {
-                $('#fileStatus').html('Please check connection! An error occured.');
-            } else if (response.responseText == '1') {
-                $('#fileStatus').html('File uploaded successfully!');
-            }
-        }
-    });
-}
-
-
 //PHOTO Uploading Function
 function profilePhoto() {
     // function from the jquery form plugin
@@ -49,6 +25,65 @@ function profilePhoto() {
         }
     });
 }
+
+
+//IMAGE Uploading Function
+function profileImage() {
+
+    // function from the jquery form plugin
+    $('#myFormPhoto').ajaxForm({
+        beforeSend: function () {
+            $('#fileStatus').html('Please wait...');
+        },
+        uploadProgress: function (event, position, total, percentComplete) {
+            $('#fileStatus').html('File transfer is going on. Please wait...');
+        },
+        success: function () {
+            $('#btnUpload').hide();
+            $('#btnNext').show();
+            $('#fileStatus').html('Picture uploaded successfully!');
+        },
+        complete: function (response) {
+
+            if (response.responseText == '0') {
+                $('#fileStatus').html('Please check connection! An error occured.');
+            } else if (response.responseText == '1') {
+                $('#fileStatus').html('Picture uploaded successfully!');
+                window.location.href = 'myAccount';
+            } else if (response.responseText == '2') {
+                $('#fileStatus').html('File size should be less than 2MB');
+            }
+        }
+    });
+
+}
+
+
+//PDF Uploading Function
+function profilePDF() {
+    // function from the jquery form plugin
+    $('#myFormPdf').ajaxForm({
+        beforeSend: function () {
+            $('#fileStatus').html('Please wait...');
+        },
+        uploadProgress: function (event, position, total, percentComplete) {
+            $('#fileStatus').html('File transfer is going on. Please wait...');
+        },
+        success: function () {
+            $('#fileStatus').html('File uploaded successfully!');
+        },
+        complete: function (response) {
+            if (response.responseText == '0') {
+                $('#fileStatus').html('Please check connection! An error occured.');
+            } else if (response.responseText == '1') {
+                $('#fileStatus').html('File uploaded successfully!');
+            }
+        }
+    });
+}
+
+
+
 
 
 //AUDIO Uploading Function
